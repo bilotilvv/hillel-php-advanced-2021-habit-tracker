8c237eaf928990f551b25d19a6d7f441c0a40527
Vitaliy Bilotil <vitaliy.bilotil@ecentria.com>
2021-11-29 18:39:27 +0200

    Added user, sign up, sign in Added mailcatcher

---
+++ src/Controller/HabitController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HabitController extends AbstractController
+{
+    /**
+     * @Route("/habits", name="app_habit")
+     */
+    public function index(): Response
+    {
+        return $this->render('habit/index.html.twig');
+    }
+}
